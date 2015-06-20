77af2307bd5fa772a3352c62bb2b0bcf2f32f455
yogeshbalan <yogeshbalan@live.co.uk>
2015-06-20 17:25:33 +0530

    Initial UI commit
    Signed-off-by: yogeshbalan <yogeshbalan@live.co.uk>

--- app/src/main/java/com/greplr/MainActivity.java
+++ app/src/main/java/com/greplr/MainActivity.java
@@ -1,28 +1,48 @@
 package com.greplr;
 
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.greplr.topcategories.TopcategoriesFragment;
+import com.quinny898.library.persistentsearch.SearchBox;
+import com.quinny898.library.persistentsearch.SearchResult;
 
 public class MainActivity extends AppCompatActivity {
 
     private static FragmentManager fragmentManager;
+    private SearchBox search;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        search = (SearchBox) findViewById(R.id.searchbox);
+        search.enableVoiceRecognition(this);
+        search.setLogoTextColor(getResources().getColor(android.R.color.white));
+
         fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.container, new TopcategoriesFragment()).commit();
-        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        getWindow().setStatusBarColor(getResources().getColor(android.R.color.transparent));
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                openSearch();
+                return true;
+            }
+        });
 
     }
 
@@ -42,14 +62,80 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public static void switchFragment(Fragment frag) {
+    public void openSearch() {
+        toolbar.setTitle("");
+        toolbar.hideOverflowMenu();
+        getSupportActionBar().hide();
+        search.revealFromMenuItem(R.id.action_search, this);
+        for (int x = 0; x < 5; x++) {
+            SearchResult option = new SearchResult("Result "
+                    + Integer.toString(x), getResources().getDrawable(
+                    R.drawable.ic_history));
+            search.addSearchable(option);
+        }
+        search.setMenuListener(new SearchBox.MenuListener() {
+
+            @Override
+            public void onMenuClick() {
+                // Hamburger has been clicked
+                Toast.makeText(MainActivity.this, "Menu click",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+        search.setSearchListener(new SearchBox.SearchListener() {
+
+            @Override
+            public void onSearchOpened() {
+                // Use this to tint the screen
+
+            }
+
+            @Override
+            public void onSearchClosed() {
+                // Use this to un-tint the screen
+                closeSearch();
+            }
+
+            @Override
+            public void onSearchTermChanged() {
+                // React to the search term changing
+                // Called after it has updated results
+            }
+
+            @Override
+            public void onSearch(String searchTerm) {
+                Toast.makeText(MainActivity.this, searchTerm + " Searched",
+                        Toast.LENGTH_LONG).show();
+                toolbar.setTitle(searchTerm);
+
+            }
+
+            @Override
+            public void onSearchCleared() {
+
+            }
+
+        });
+
+    }
+
+    protected void closeSearch() {
+        toolbar.showOverflowMenu();
+        getSupportActionBar().show();
+        search.hideCircularly(this);
+        if(search.getSearchText().isEmpty())toolbar.setTitle("Greplr");
+    }
+
+    public static void switchFragment(Fragment frag, View view, String name) {
+        fragmentManager.executePendingTransactions();
         fragmentManager.beginTransaction().replace(R.id.container, frag).addToBackStack("main").commit();
     }
 
