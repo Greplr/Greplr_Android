efabd96fd36a8cf271a066bae933b31c4f1fc1a5
Prempal Singh <prempal.42@gmail.com>
2015-07-08 00:41:37 +0530

    internet check

--- app/src/main/java/com/greplr/common/utils/Utils.java
+++ app/src/main/java/com/greplr/common/utils/Utils.java
@@ -111,4 +111,15 @@ public class Utils {
             }
         });
     }
+
+    public static boolean isOnline() {
+        try {
+            Process process = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
+            int returnVal = process.waitFor();
+            return returnVal==0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
